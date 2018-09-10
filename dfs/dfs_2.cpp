4c594481bfc4fc8c3f5da1c182a76884cfb39831
makehoney <makehoney3@gmail.com>
2018-09-11 01:03:21 +0900

    add dfs_2(when computing the number of graph)

---
+++ dfs/dfs_2.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <vector>
+
+#define MAX 2000
+using namespace std;
+
+vector<int> graph[MAX];
+bool visit[MAX];
+bool flag;
+
+void dfs(int start) {
+    visit[start] = true;
+
+    int next;
+    for(int i = 0; i < graph[start].size(); i++) {
+        next = graph[start][i];
+        if(!visit[next]) dfs(next);
+    }
+}
+
+int main() {
+    int v, testCase, node;
+    int cnt = 0;
+    scanf("%d", &testCase);
+
+    for(int k = 0; k < testCase; k++) {
+        scanf("%d", &node);
+        flag = false; cnt = 0;
+        for(int i = 1; i <= node; i++) {
+            scanf("%d", &v);
+            graph[i].push_back(v);
+        }
+
+        for(int i = 1; i <= node; i++) {
+            /* 그래프 개수 구할 때 */
+            if(!visit[i]) cnt++;
+            dfs(i);
+        }
+        for(int i = 1; i <= node; i++) {
+            graph[i].clear();
+            visit[i] = false;
+        }
+        printf("%d\n", cnt);
+    }
+}
